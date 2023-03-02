a3569af49ce53a5d6685ba53dc9a912c793a1b47
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-02 20:36:01 +0900

    implement http trigger

--- src/main/java/com/github/miyohide/HttpTriggerJava1.java
+++ src/main/java/com/github/miyohide/HttpTriggerJava1.java
@@ -1,6 +1,10 @@
 package com.github.miyohide;
 
 import java.util.*;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
@@ -8,13 +12,11 @@ import com.microsoft.azure.functions.*;
  * Azure Functions with HTTP Trigger.
  */
 public class HttpTriggerJava1 {
-    /**
-     * This function listens at endpoint "/api/HttpTriggerJava1". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTriggerJava1
-     * 2. curl {your host}/api/HttpTriggerJava1?name=HTTP%20Query
-     */
+    @Autowired
+    private Function<String, String> uppercase;
+
     @FunctionName("HttpTriggerJava1")
-    public HttpResponseMessage run(
+    public String run(
             @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
@@ -24,9 +26,9 @@ public class HttpTriggerJava1 {
         String name = request.getBody().orElse(query);
 
         if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+            return uppercase.apply("Please input your name...");
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return uppercase.apply(name);
         }
     }
 }
