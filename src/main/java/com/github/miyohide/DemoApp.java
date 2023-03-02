d8d1d576858f03414e10dc84e8a4c67a1e354759
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-02 20:35:45 +0900

    create main method and bean

---
+++ src/main/java/com/github/miyohide/DemoApp.java
@@ -0,0 +1,22 @@
+package com.github.miyohide;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApp {
+    @Bean
+    public Function<String, String> uppercase() {
+        return payload -> {
+            String output = payload.toUpperCase();
+            return String.format("Input: %s", output);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+}
