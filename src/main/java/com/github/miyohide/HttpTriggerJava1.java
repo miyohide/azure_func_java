fa47d56cd1bd5471ce11a0fcc514f32cb5fcbe09
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-04 17:40:14 +0900

    fix func

--- src/main/java/com/github/miyohide/HttpTriggerJava1.java
+++ src/main/java/com/github/miyohide/HttpTriggerJava1.java
@@ -1,16 +1,19 @@
 package com.github.miyohide;
 
-import java.util.*;
+import java.util.Optional;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
+@Component
 public class HttpTriggerJava1 {
     @Autowired
     private Function<String, String> uppercase;
@@ -21,14 +24,6 @@ public class HttpTriggerJava1 {
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
-
-        if (name == null) {
-            return uppercase.apply("Please input your name...");
-        } else {
-            return uppercase.apply(name);
-        }
+        return uppercase.apply(request.getBody().orElse("Hello World"));
     }
 }
