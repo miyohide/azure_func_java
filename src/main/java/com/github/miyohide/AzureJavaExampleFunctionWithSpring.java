dcf490667320ec233127c8165159bc27d1715aa7
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-05 12:45:53 +0900

    re-write

--- src/main/java/com/github/miyohide/AzureJavaExampleFunctionWithSpring.java
+++ src/main/java/com/github/miyohide/AzureJavaExampleFunctionWithSpring.java
@@ -2,28 +2,27 @@ package com.github.miyohide;
 
 import java.util.Optional;
 import java.util.function.Function;
-
-import org.springframework.stereotype.Component;
-
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class AzureJavaExampleFunctionWithSpring {
-    private final Function<String, String> uppercase;
 
-    public AzureJavaExampleFunctionWithSpring(Function<String, String> uppercase) {
-        this.uppercase = uppercase;
-    }
+   @Autowired
+   private Function<String, String> uppercase;
 
-    @FunctionName("example")
-    public String execute(
-        @HttpTrigger(name = "req", methods = { HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-        ExecutionContext context) {
-            return uppercase.apply("Hello World");
-        }
+   @FunctionName("example")
+   public String execute(
+           @HttpTrigger(name = "req", methods = { HttpMethod.GET,
+                   HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+           ExecutionContext context) {
+
+       return uppercase.apply(request.getBody().orElse("Hello World"));
+   }
 }
