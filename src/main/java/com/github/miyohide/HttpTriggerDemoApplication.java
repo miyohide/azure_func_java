8df4ff7e3469d8c5fdbd142838e2bf09ac3bf711
Hidenori Miyoshi <miyohide@gmail.com>
2023-03-06 21:00:07 +0900

    run splotless:apply

--- src/main/java/com/github/miyohide/HttpTriggerDemoApplication.java
+++ src/main/java/com/github/miyohide/HttpTriggerDemoApplication.java
@@ -8,15 +8,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class HttpTriggerDemoApplication {
 
-   @Bean
-   public Function<String, String> uppercase() {
-       return payload -> {
-           String output = payload.toUpperCase();
-           return String.format("Input: %s", output);         
-       };
-   }
+    @Bean
+    public Function<String, String> uppercase() {
+        return payload -> {
+            String output = payload.toUpperCase();
+            return String.format("Input: %s", output);
+        };
+    }
 
-   public static void main(String[] args) {
-       SpringApplication.run(HttpTriggerDemoApplication.class, args);
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(HttpTriggerDemoApplication.class, args);
+    }
 }
--- src/main/java/com/github/miyohide/MyHttpTrigger.java
+++ src/main/java/com/github/miyohide/MyHttpTrigger.java
@@ -1,28 +1,30 @@
 package com.github.miyohide;
 
-import java.util.Optional;
-import java.util.function.Function;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import java.util.Optional;
+import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyHttpTrigger {
 
-   @Autowired
-   private Function<String, String> uppercase;
+    @Autowired private Function<String, String> uppercase;
 
-   @FunctionName("example")
-   public String execute(
-           @HttpTrigger(name = "req", methods = { HttpMethod.GET,
-                   HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-           ExecutionContext context) {
+    @FunctionName("example")
+    public String execute(
+            @HttpTrigger(
+                            name = "req",
+                            methods = {HttpMethod.GET, HttpMethod.POST},
+                            authLevel = AuthorizationLevel.ANONYMOUS)
+                    HttpRequestMessage<Optional<String>> request,
+            ExecutionContext context) {
 
-       return uppercase.apply(request.getBody().orElse("Hello World"));
-   }
+        return uppercase.apply(request.getBody().orElse("Hello World"));
+    }
 }
